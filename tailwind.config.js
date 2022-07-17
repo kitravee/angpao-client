ec232b0fab277913b96ab8a81dfb5b25a242e0d1
kitravee <kitravee.swk@gmail.com>
2022-07-17 12:49:09 +0700

    fix: add fontfam and color base on design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ap1: '#DB5229',
+        ap2: '#FFBB16',
+        ap3: '#E3D645',
+        ap4: '#53A86F',
+        ap5: '#1F685C',
+      },
+      fontFamily: {
+        sans: ['Prompt', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
