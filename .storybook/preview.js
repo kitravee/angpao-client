2c20f79293acd00242006b1fe2e985ce27a936ef
kitravee <kitravee.swk@gmail.com>
2022-08-21 18:19:08 +0700

    fix: story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,29 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import theme from '../src/theme/theme';
+import { ThemeProvider } from '@mui/material/styles';
+import { createEmotionCache, EmotionCacheProvider } from '../src/lib/emotion';
+import { CssBaseline } from '@mui/material';
+
+const clientSideEmotionCache = createEmotionCache();
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+const withThemeProvider = (Story, context) => (
+  <EmotionCacheProvider emotionCache={clientSideEmotionCache}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Story {...context} />
+    </ThemeProvider>
+  </EmotionCacheProvider>
+);
+
+export const decorators = [withThemeProvider];
