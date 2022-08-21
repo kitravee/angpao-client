edea6ff54e4acae76499d822c90346b0ae72db66
kitravee <kitravee.swk@gmail.com>
2022-08-21 15:11:40 +0700

    fix: storybook

--- src/theme/myfirst.stories.tsx
+++ src/theme/myfirst.stories.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@mui/material';
+import { Box } from '@mui/system';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import React from 'react';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'System/Button',
+  title: 'SystemButton',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
@@ -22,18 +23,42 @@ export default {
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => (
-  <>
-    <Button {...args}>Button</Button>
-    <Button {...args}>Button</Button>
-    <Button {...args}>Button</Button>
-    <Button {...args}>Button</Button>
-  </>
+  <Box>
+    <Box sx={{ '& > button': { mx: 0.5 } }}>
+      <Button size="small" {...args}>
+        small
+      </Button>
+      <Button size="medium" {...args}>
+        medium
+      </Button>
+      <Button size="large" {...args}>
+        large
+      </Button>
+    </Box>
+    <Box sx={{ '& > button': { mx: 0.5 } }}>
+      <Button size="small" variant="outlined" {...args}>
+        small
+      </Button>
+      <Button size="medium" variant="outlined" {...args}>
+        medium
+      </Button>
+      <Button size="large" variant="outlined" {...args}>
+        large
+      </Button>
+    </Box>
+    <Box sx={{ '& > button': { mx: 0.5 } }}>
+      <Button size="small" variant="contained" {...args}>
+        small
+      </Button>
+      <Button size="medium" variant="contained" {...args}>
+        medium
+      </Button>
+      <Button size="large" variant="contained" {...args}>
+        large
+      </Button>
+    </Box>
+  </Box>
 );
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  color: 'primary',
-  variant: 'outlined',
-  size: 'large',
-};
