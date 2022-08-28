03f70d6b6aef4bc41edeb4a4127f099f53ab276e
kitravee <kitravee.swk@gmail.com>
2022-08-28 20:59:59 +0700

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
+const nextPlugins = [withPWA];
+
+module.exports = withPlugins([nextConfig, nextPlugins]);
